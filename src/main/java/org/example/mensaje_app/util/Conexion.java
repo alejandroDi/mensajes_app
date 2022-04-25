6b41f692b27051b9c4d95051dbf04391211069a3
yonat <yonathan.tovar@alumnos.udg.mx>
2022-04-25 13:02:31 -0500

    mi primer commit

---
+++ src/main/java/org/example/mensaje_app/util/Conexion.java
@@ -0,0 +1,22 @@
+package org.example.mensaje_app.util;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion {
+
+    public Connection get_connetion(){
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5433/mensajes_app","postgres","Postgres2022$");
+            if (connection != null){
+                System.out.println("conexion exitosaa");
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+        return connection;
+    }
+}
